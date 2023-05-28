45ece87e21b969e885e612fe20cd70cab61a24a6
Ckatt <Ckatt@ukr.net>
2023-05-28 22:12:45 +0300

    added method sysOut

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,4 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    public void sysOut(){
+        System.out.println("Hello Vitaliy");
+    }
 }
