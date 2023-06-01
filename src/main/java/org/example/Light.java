3a88595c4e269fcc9474f9b809437451983042ec
Ckatt <Ckatt@ukr.net>
2023-06-01 20:00:48 +0300

    Added a new class Light

---
+++ src/main/java/org/example/Light.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class Light {
+
+    public void on(){
+
+    }
+
+    public void off(){
+
+    }
+    public void brighten(){
+
+    }
+    public void dimm(){
+
+    }
+
+    
+
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,6 @@
 package org.example;
 
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
@@ -16,6 +17,9 @@ public class Main {
     public void vitaliyOut(){
         System.out.println("Vitaliy");
     }
+
+    Light lt = new Light();
+
 
 
 }
