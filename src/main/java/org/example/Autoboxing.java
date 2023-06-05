dfcfe18e2af2624f3568e44a2135dc394767d217
Ckatt <Ckatt@ukr.net>
2023-06-05 21:02:52 +0300

    added class Autoboxing

---
+++ src/main/java/org/example/Autoboxing.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Autoboxing {
+    // Упаковка простых типов
+    // Класы обертки позволяют создать в куче не примитивный обьект для представления примитивного типа
+    char c = 'x' ;
+    Character ch = new Character(c);
+
+    Character сh = new Character('x');
+
+    // и обратно
+    char cw = ch;
+
+
+
+}
