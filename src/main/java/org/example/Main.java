6743eceaea61ab6de1ebaa4d9a378b505dee9c93
Ckatt <Ckatt@ukr.net>
2023-06-07 18:51:19 +0300

    added method in Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -20,6 +20,25 @@ public class Main {
 
     Light lt = new Light();
 
+// Метод указывает сколько байт необходимо для хранения строки
+    int storage(String s){
+        return s.length();
+    }
+
+    public boolean flag (){
+        return true;
+    }
+
+    public float naturalLogBase(){
+        return 2.718F;
+    }
+
+    public void nothing (){
+        return;
+    }
+    public void nothing2(){
+
+    }
 
 
 }
