66b6df8494446e2e72a798c73920887cdbcd3774
Ckatt <Ckatt@ukr.net>
2023-06-25 16:10:22 +0300

    home work part 5 v 3

---
+++ src/main/java/org/example/homeWork/part5/number1/number3/Apple.java
@@ -0,0 +1,13 @@
+package org.example.homeWork.part5.number1.number3;
+
+public class Apple {
+    public Apple(){
+
+            System.out.println("Создан обьект с помощью конструктора");
+
+    }
+
+    public static void main(String[] args){
+        Apple apple = new Apple();
+    }
+}
